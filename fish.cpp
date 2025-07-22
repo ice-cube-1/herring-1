09060b74a4d6dbbaf125b8cebec5add45e5febbd
ice-cube-1 <alice.goward@spgs.org>
2025-07-22 16:54:07 +0100

    clamping

--- fish.cpp
+++ fish.cpp
@@ -4,7 +4,7 @@
 #include <random>
 
 #define d_t 0.1
-#define fishCount 50
+#define fishCount 10
 
 #define sigma 0.1
 #define alpha 0.05
@@ -12,7 +12,9 @@
 #define r 0.9  
 #define p 2.5    
 #define q 1.5       
-
+#define max_dv 1.5 * d_t
+#define max_v 0.6
+#define min_v 0.03
 
 #define dimensions 2
 
@@ -26,7 +28,7 @@ class Fish {
     int uid;
     Fish () { 
         for (int i = 0; i<dimensions; i++) {
-            x[i] = (rand()%400 + 200)/10.0f;
+            x[i] = (rand()%400 + 200)/40.0f;
             v[i] = (rand() % 10 - 5) / 5.0f; 
         }
         uid = rand()%1000;
@@ -40,6 +42,14 @@ float get_abs_difference(float a[dimensions], float b[dimensions]) {
     float ans = 0;
     for (int i = 0; i<dimensions; i++) {
         ans += std::pow(a[i]-b[i],2);
+    }
+    return std::pow(ans, 0.5);
+}
+
+float vector_abs(float a[dimensions]) {
+    float ans = 0;
+    for (int i = 0; i<dimensions; i++) {
+        ans += std::pow(a[i],2);
     }
     return std::pow(ans, 0.5);
 }
@@ -75,9 +85,30 @@ int main()
                     }
                 }
             }
+            float abs_dv = vector_abs(dv);
+            if (abs_dv > max_dv) {
+                float scale_factor = max_dv / abs_dv;
+                for (int dimension = 0; dimension < dimensions; dimension++) {
+                    dv[dimension] *= scale_factor;
+                }
+            }
 
             for (int dimension = 0; dimension<dimensions; dimension++) {
                 fishes[i].v[dimension] += dv[dimension]*d_t;
+            }
+
+            float abs_v = vector_abs(fishes[i].v);
+            float scale_factor = 1;
+            if (abs_v > max_v) {
+                scale_factor = max_v / abs_v;
+            } else if (abs_v < min_v) {
+                scale_factor = min_v / abs_v;
+            }
+            for (int dimension = 0; dimension < dimensions; dimension++) {
+                fishes[i].v[dimension] *= scale_factor;
+            }
+
+            for (int dimension = 0; dimension<dimensions; dimension++) {
                 float dx = distribution(generator)*sigma + fishes[i].v[dimension];
                 fishes[i].x[dimension] += dx*d_t;
                 std::cout << fishes[i].v[dimension] << dv[dimension] * d_t << "\n";
@@ -86,7 +117,7 @@ int main()
 
             sf::CircleShape circle(1.f);
             circle.setFillColor(sf::Color{0, 150, 255, fishes[i].color_alpha()});
-            circle.setPosition(fishes[i].x[0]*10, fishes[i].x[1]*10);
+            circle.setPosition(fishes[i].x[0]*40, fishes[i].x[1]*40);
             window.draw(circle);
         }
         window.display();
