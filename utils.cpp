b401d4c23b0eab9ca065242f88bfd26f17126ab0
ice-cube-1 <alice.goward@spgs.org>
2025-07-25 19:29:34 +0100

    improved tank edge detection (such that obstacles are possible)

--- utils.cpp
+++ utils.cpp
@@ -10,31 +10,60 @@ void remove_unordered(std::vector<Herring*>& vec, Herring* value) {
     }
 }
 
-void avoidTank(const Vec3& s, const Vec3& v, Vec3& a) {
-    for (int dimension = 0; dimension<dimensions; dimension ++) {
-        Vec3 reflection_vector;
-        for (int d = 0; d < dimensions; d++) {
-            if (d == dimension) { reflection_vector.arr[d] = -v.arr[d]; }
-            else { reflection_vector.arr[d] = v.arr[d]; }
-        }
-        Vec3 reflection_pos;
-        if (v.arr[dimension] < 0) {
-            reflection_pos.arr[dimension] = 0;
-        } else {
-            reflection_pos.arr[dimension] = tank_size;
-        }
-        for (int other_d = 0; other_d < dimensions; other_d ++) {
-            if (other_d != dimension) {
-                float divisor = (reflection_pos.arr[dimension] - v.arr[dimension]);
-                if (abs(divisor) < epsilon) { divisor = epsilon; }
-                reflection_pos.arr[other_d] = (s.arr[dimension] * v.arr[other_d])/divisor + s.arr[other_d];
+class Plane {
+    public:
+    int dimension;
+    Plane() {}
+    Plane(Vec3 min, Vec3 max, int d) { min_values = min; max_values = max; dimension = d; }
+
+    float distance_to_intersection(Vec3 s, Vec3 v) {
+        if ((v.arr[dimension] > 0 && s.arr[dimension] > min_values.arr[dimension]) ||
+            (v.arr[dimension] < 0 && s.arr[dimension] < min_values.arr[dimension]))
+            return -1;
+
+        Vec3 meeting_point;
+        meeting_point.arr[dimension] = min_values.arr[dimension];
+        for (int i = 0; i<3; i++) {
+            if (i!=dimension) {
+                meeting_point.arr[i] = ((meeting_point.arr[dimension]-s.arr[dimension])*v.arr[i]/v.arr[dimension])+s.arr[i];
+                if (meeting_point.arr[i] > max_values.arr[i] || meeting_point.arr[i] < min_values.arr[i]) {
+                    return -1;
+                }
             }
         }
-        float abs_distance = (reflection_pos - s).abs();
-        if (abs_distance < epsilon) {
-            abs_distance = epsilon;
+        return (s-meeting_point).abs();
+    }
+    private:
+    Vec3 min_values;
+    Vec3 max_values;
+};
+
+void avoidTank(const Vec3& s, const Vec3& v, Vec3& a) {
+    Plane planes[6] = {
+        Plane(Vec3{0,0,0}, Vec3{0,tank_size,tank_size}, 0),
+        Plane(Vec3{tank_size,0,0}, Vec3{tank_size,tank_size,tank_size}, 0),
+        Plane(Vec3{0,0,0}, Vec3{tank_size,0,tank_size}, 1),
+        Plane(Vec3{0,tank_size,0}, Vec3{tank_size,tank_size,tank_size}, 1),
+        Plane(Vec3{0,0,0}, Vec3{tank_size,tank_size,0}, 2),
+        Plane(Vec3{0,0,tank_size}, Vec3{tank_size,tank_size,tank_size}, 2)
+    };
+    Plane closest_plane;
+    float closest_plane_dist = std::numeric_limits<float>::max();
+    for (Plane plane: planes) {
+        float dist_intersect = plane.distance_to_intersection(s,v);
+        if (dist_intersect != -1 && dist_intersect < closest_plane_dist) {
+            closest_plane = plane;
+            closest_plane_dist = dist_intersect;
         }
-        float scalar_collision = r_p/std::pow(abs_distance,p) + r_q/std::pow(abs_distance,q);
+    }
+
+    if (closest_plane_dist > epsilon) { 
+        Vec3 reflection_vector;
+        for (int d = 0; d < dimensions; d++) {
+            reflection_vector.arr[d] = (d == closest_plane.dimension) ? -v.arr[d] : v.arr[d];
+        }
+
+        float scalar_collision = r_p/std::pow(closest_plane_dist,p) + r_q/std::pow(closest_plane_dist,q);
         if (std::isfinite(scalar_collision)) {
             a = a - (v - reflection_vector) * (gamma * scalar_collision);
         }
