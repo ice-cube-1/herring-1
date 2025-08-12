fd46ed02b6731c89ef1c81a47ed4fc6dc6e4e11b
Alice Goward <alice.goward@spgs.org>
2025-08-12 14:27:15 +0100

    more analysis

--- main.cpp
+++ main.cpp
@@ -158,7 +158,7 @@ class Param {
     Param(std::string n, float l, float u, float d) {name = n, lower_bound = l; upper_bound = u; default_value = d; }
 };
 
-std::array<Param, dim_prey> params_prey = {Param("α",-3, 6, 0.2), Param("β",-3,6,0.5), Param("γ",0,5,0.5), Param("δ",epsilon,0.005,0.0001)};
+std::array<Param, dim_prey> params_prey = {Param("α",-3, 3, 0.2), Param("β",-3,3,0.5), Param("γ",0,5,0.5), Param("δ",1e-8,0.0005,0.00001)};
 std::array<Param, dim_predator> params_predator = {Param("γ1",epsilon, 6, 2), Param("γ2",epsilon,6,0.5), Param("k",-1,1,0.5)};
 
 void print_arr(const std::array<double,dim_prey>&x, const std::array<double,dim_predator>&y) {
